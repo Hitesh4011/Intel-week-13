60f8df6dc1f58452549b28054a048f84f4bf57e8
Hitesh4011 <153000329+Hitesh4011@users.noreply.github.com>
2025-05-17 17:08:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ from PIL import Image
 import os
 import streamlit as st
 
+
+
+st.set_page_config(page_title="Product Defect Detection", layout="wide")
 try:
     import tensorflow as tf
     print("✅ TensorFlow imported successfully.")
 except Exception as e:
     print("❌ TensorFlow crashed:", e)
-
-st.set_page_config(page_title="Product Defect Detection", layout="wide")
-
+    st.write(e)
 st.title("📦 Product Defect Detection")
 st.write("Upload an image of the product to check if it is Normal or Defective.")
 
